83f469b8d0631a41fc1526e3e5f71655ee5ac9cd
kfilip10 <147560289+kfilip10@users.noreply.github.com>
2023-12-17 08:28:52 -0500

    Updated to test electron squirrel udater

--- forge.config.js
+++ forge.config.js
@@ -7,6 +7,9 @@ module.exports = {
     {
       name: '@electron-forge/maker-squirrel',
       config: {},
+      "platforms": [
+        "win32"
+      ]
     },
     {
       name: '@electron-forge/maker-zip',
@@ -27,4 +30,16 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "kfilip10",
+          name: "EF-test"
+        },
+        prerelease: true
+      }
+    }
+  ]
 };
